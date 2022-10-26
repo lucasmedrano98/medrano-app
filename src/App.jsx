03831c0ed05fbc7f5072e25642def01c5e00d219
Lucas Medrano <lucasmedrano98.lm@gmail.com>
2022-10-26 16:41:38 -0300

    Se corrigio firebase otra vez

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar1 from "./components/Navbar";
 import ItemListContainer from "./container/itemListContainer.jsx";
 import ItemDetailContainer from "./container/ItemDetailContainer";
+import ItemListCategory from "./container/itemListCategory.jsx";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import CarritoProvider from "./context/CarritoContext";
 import Cart from "./components/Cart";
@@ -13,7 +14,7 @@ function App() {
           <Navbar1/>
           <Routes>
             <Route path="/" element={<ItemListContainer/>}/>
-            <Route path="/categoria/:idCategoria" element={<ItemListContainer/>}/>
+            <Route path="/categoria/:idCategoria" element={<ItemListCategory/>}/>
             <Route path="/producto/:idProduct" element={<ItemDetailContainer/>}/>
             <Route path="/cart" element={<Cart/>}/>
           </Routes>
--- src/container/itemListCategory.jsx
+++ src/container/itemListCategory.jsx
@@ -1,19 +1,21 @@
 import { useEffect } from "react";
 import { useState } from 'react';
 import ItemContainer from './ItemContainer'
-import { collection, getDocs, getFirestore} from "firebase/firestore";
+import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
+import { useParams } from "react-router-dom";
 
-const ItemListContainer = () => {
+const ItemListCategory = ({setCarrito, carrito}) => {
     const [productos, setProductos] = useState ([])
+    const {idCategoria} = useParams();
     
     useEffect(() => {
         const db = getFirestore();
-        const itemsCollection = collection (db, "items");
-        getDocs(itemsCollection).then((snapshot) => {
+        const q = query(collection (db, "items"), where("categoryId", "==", `${idCategoria}`));
+        getDocs(q).then((snapshot) => {
             const docs = (snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})));
             setProductos(docs)
         });
-    },[productos])
+    },[{idCategoria}])
 
 
         // useEffect(() => {
@@ -27,10 +29,10 @@ const ItemListContainer = () => {
 
 
         return (
-            <ItemContainer productos={productos}/>
+            <ItemContainer productos={productos} setCarrito={setCarrito} carrito={carrito}/>
             
         )
     }
 
 
-export default ItemListContainer
+export default ItemListCategory
